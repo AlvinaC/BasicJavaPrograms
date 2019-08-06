352a19e3d322d854e0928fa763e93f00e7248e5d
Alvina <alvina13.rasquinha@gmail.com>
2019-08-06 07:50:57 +0530

    Added new programs

--- src/com/example/programs/CHFM.java
+++ src/com/example/programs/CHFM.java
@@ -1,36 +1,49 @@
 package com.example.programs;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
-
-//https://www.codechef.com/JULY19B/problems/CHFM
+import java.util.Collections;
 
 public class CHFM {
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		try {
-			Scanner s = new Scanner(System.in);
-			int tc = s.nextInt();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			int tc = Integer.parseInt(reader.readLine().trim());
+			CHFM c = new CHFM();
 			ArrayList<Long> list = new ArrayList<Long>();
 			for (int i = 0; i < tc; i++) {
-				int n = s.nextInt();
-				ArrayList<Long> a = new ArrayList<Long>();
+				int n = Integer.parseInt(reader.readLine().trim());
 				double sum = 0;
-				for (int j = 0; j < n; j++) {
-					a.add(s.nextLong());
-					sum = sum + a.get(j);
+				String[] integersInString = reader.readLine().split(" ");
+				long[] a = new long[n];
+				for (int j = 0; j < integersInString.length; j++) {
+					a[j] = Long.parseLong(integersInString[j]);
+					sum = sum + a[j];
 				}
 				double mean = sum / n;
-				double remainder = sum % n;
-				if (remainder != 0) {
-					list.add(-1L);
-					continue;
+				int h = 0;
+				ArrayList<Long> mylist = new ArrayList<>();
+				while (h < n) {
+					double newMean = (mean * n - a[h]) / (n - 1);
+					if (newMean == mean)
+						mylist.add(new Long(h + 1));
+					h++;
 				}
-				
-
+				if (mylist.size() > 0)
+					list.add(mylist.get(0));
+				else
+					list.add(-1l);
+			}
+			for (int i = 0; i < tc; i++) {
+				if (list.get(i) != -1)
+					System.out.println(list.get(i));
+				else
+					System.out.println("Impossible");
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
+
 }
